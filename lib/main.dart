f4ee7f7cabdb8e9f9cea79671a1bc70fda39267e
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-04-05 22:48:06 +0530

    Updated the main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,116 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(TipApp());
 
-class MyApp extends StatelessWidget {
+class TipApp extends StatelessWidget {
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Tip App',
+      theme: ThemeData(primaryColor: Colors.purple),
+      home: TipCalculator(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class TipCalculator extends StatefulWidget {
+  // This widget is the home screen of the Tip Calculator app. It is stateful, since we want
+  // to keep track of the TipAmount & TotalAmount and update the UI accordingly
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _TipCalculatorState createState() => _TipCalculatorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  double bill = 0;
-  double tipPercentage = 15;
-  double tipAmount = 0;
-  double totalAmount = 0;
+class _TipCalculatorState extends State<TipCalculator> {
+  static const defaultBillAmount = 0.0;
 
-  void tipCal() {
-    tipAmount = bill * tipPercentage * 0.01;
-    totalAmount = bill + tipAmount;
-    print(tipAmount);
-    print(totalAmount);
-    setState(() {});
+  static const defaultTipPercentage = 15;
+
+  final _billAmountController =
+      TextEditingController(text: defaultBillAmount.toString());
+
+  final _tipPercentageController =
+      TextEditingController(text: defaultTipPercentage.toString());
+
+  double _billAmount = defaultBillAmount;
+
+  int _tipPercentage = defaultTipPercentage;
+
+  @override
+  void initState() {
+    super.initState();
+    _billAmountController.addListener(_onBillAmountChanged);
+    _tipPercentageController.addListener(_onTipAmountChanged);
   }
+
+  _onBillAmountChanged() {
+    setState(() {
+      _billAmount = double.tryParse(_billAmountController.text) ?? 0.0;
+    });
+  }
+
+  _onTipAmountChanged() {
+    setState(() {
+      _tipPercentage = int.tryParse(_tipPercentageController.text) ?? 0;
+    });
+  }
+
+  _getTipAmount() => _billAmount * _tipPercentage / 100;
+
+  _getTotalAmount() => _billAmount + _getTipAmount();
 
   @override
   Widget build(BuildContext context) {
+    // This method builds the UI of the Tip Calculator.
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Tip Calculator!'),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                decoration: InputDecoration(hintText: "Enter bill amount"),
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  print(value);
-                  value != "" ? bill = double.parse(value) : bill = 0.0;
-                },
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(hintText: "Enter tip percentage"),
-                onChanged: (value) {
-                  print(value);
-                  setState(() {
-                    value != null
-                        ? tipPercentage = double.parse(value)
-                        : tipPercentage = 0.0;
-                  });
-                },
-              ),
-              Row(
-                children: [
-                  Text("Tip Amount"),
-                  Spacer(),
-                  Text("$tipAmount"),
-                ],
-              ),
-              Row(
-                children: [
-                  Text("Total Amount"),
-                  Spacer(),
-                  Text("$totalAmount"),
-                ],
-              ),
-              RaisedButton(
-                onPressed: () {
-                  tipCal();
-                },
-                child: Text("Calculate tip"),
-              )
-            ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Form(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  key: Key("billAmount"),
+                  controller: _billAmountController,
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  decoration: InputDecoration(
+                    hintText: 'Enter the Bill Amount',
+                    labelText: 'Bill Amount',
+                  ),
+                ),
+                TextFormField(
+                  key: Key("tipPercentage"),
+                  controller: _tipPercentageController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    hintText: 'Enter the Tip %',
+                    labelText: 'Tip %',
+                  ),
+                ),
+                Text(
+                  'Tip Amount: ${_getTipAmount()}',
+                  key: Key('tipAmount'),
+                ),
+                Text(
+                  'Total Amount: ${_getTotalAmount()}',
+                  key: Key('totalAmount'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _billAmountController.dispose();
+    _tipPercentageController.dispose();
+    super.dispose();
   }
 }
